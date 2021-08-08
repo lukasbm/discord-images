440d6b4aa9de6d9d3fc4d027dbab6c2a52404562
Lukas <boehm-lukas@outlook.de>
2021-08-09 00:26:24 +0200

    :sparkles: empty bot works

--- bot/bot.ts
+++ bot/bot.ts
@@ -6,20 +6,18 @@ export class Bot {
   private static _client: Client;
 
   static start() {
+    console.log("start");
+
     this._client = new Client();
-    // In the login method, you must specify the glob string to load your classes (for the framework).
-    // In this case that's not necessary because the entry point of your application is this file.
     this._client.login(
       process.env.DISCORD_TOKEN,
       `${__dirname}/*Discord.ts` // glob string to load the classes
     );
+    this._client.login(process.env.DISCORD_TOKEN);
   }
 
-  @On("message")
-  async onMessage(message: Message, client: Client) {
-    console.log(message);
-  }
+  // @On("message")
+  // async onMessage(message: Message, client: Client) {
+  //   console.log(message);
+  // }
 }
-
-// Start your app
-Bot.start();
--- bot/main.ts
+++ bot/main.ts
@@ -1,18 +1,3 @@
-// import { Client } from "@typeit/discord";
+import { Bot } from "./bot";
 
-function start() {
-  console.log("lets gooo");
-
-  // const client = new Client({
-  //   classes: [
-  //     `${__dirname}/*Discord.ts`, // glob string to load the classes
-  //     `${__dirname}/*Discord.js`, // If you compile using "tsc" the file extension change to .js
-  //   ],
-  //   silent: false,
-  //   variablesChar: ":",
-  // });
-
-  // await client.login("YOUR_TOKEN");
-}
-
-start();
+Bot.start();
