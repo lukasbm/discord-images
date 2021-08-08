bc71c06f6208038f0deb928f31d7562631b36ac0
Lukas <lukas.l.boehm@fau.de>
2021-08-08 04:10:00 +0200

    :truck: use some ES6 modules

--- bot/index.js
+++ bot/index.js
@@ -1,44 +1,5 @@
-const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+import { analyzeImage } from "./analyze.js";
 
-const stub = ClarifaiStub.grpc();
-
-const metadata = new grpc.Metadata();
-metadata.set("authorization", `Key ${process.env.CLARIFAI_API_KEY}`);
-
-stub.PostModelOutputs(
-  {
-    // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
-    model_id: "aaa03c23b3724a16a56b629203edc62c",
-    inputs: [
-      {
-        data: {
-          image: {
-            url: "https://media.discordapp.net/attachments/642925836630097930/873605082313277450/20210805_194903.jpg",
-          },
-        },
-      },
-    ],
-  },
-  metadata,
-  (err, response) => {
-    if (err) {
-      console.log("Error: " + err);
-      return;
-    }
-
-    if (response.status.code !== 10000) {
-      console.log(
-        "Received failed status: " +
-          response.status.description +
-          "\n" +
-          response.status.details
-      );
-      return;
-    }
-
-    console.log("Predicted concepts, with confidence values:");
-    for (const c of response.outputs[0].data.concepts) {
-      console.log(c.name + ": " + c.value);
-    }
-  }
+analyzeImage(
+  "https://media.discordapp.net/attachments/642925836630097930/873605082313277450/20210805_194903.jpg"
 );
