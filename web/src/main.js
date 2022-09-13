fc93c737b4b122cf895275859d443340350db8e1
Lukas <boehm-lukas@outlook.de>
2022-09-14 00:24:17 +0200

    :sparkles: add masonry

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./assets/main.css";
-import * as bootstrap from "bootstrap";
+import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "masonry-layout";
 
 createApp(App).mount("#app");
