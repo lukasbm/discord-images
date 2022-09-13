00e11db894fceafee161e7980939bc6f24ed6368
Lukas <boehm-lukas@outlook.de>
2022-09-14 00:16:49 +0200

    :lipstick: add bootstrap

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1>test</h1>
+  <div class="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-primary">Primary button</button>
+  </div>
 </template>
--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./assets/main.css";
+import * as bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createApp(App).mount("#app");
