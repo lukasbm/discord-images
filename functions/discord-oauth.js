efe2011f246ed5d4d042033fe0fb95ed42a0e1aa
Lukas <boehm-lukas@outlook.de>
2022-09-14 00:24:40 +0200

    :construction: empty cloud function

---
+++ functions/discord-oauth.js
@@ -0,0 +1,7 @@
+// TODO fetch discord client secret stored in firebase secrets
+
+// Create token after sign in with discord
+
+// The frontend will receive this token upon authentication
+
+// and will use it to pose real authenticated requests again the protected firestore database.
