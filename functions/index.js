94aeba7656dbd3bcdeef42f744396ceccf087f9f
Lukas <boehm-lukas@outlook.de>
2022-09-18 18:18:14 +0200

    :sparkles: add error handling to cloud function

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,6 @@ const app = initializeApp(adminConfig);
 const auth = getAuth(app);
 
 const discordApiBase = "https://discord.com/api/v10/";
-
 const clientId = process.env.DISCORD_CLIENT_ID;
 const clientSecret = process.env.DISCORD_CLIENT_SECRET;
 
@@ -23,68 +22,86 @@ const discordCodeExchange = async (authCode, redirectUri) => {
   params.append("code", authCode);
   params.append("redirect_uri", redirectUri);
 
-  let response = await fetch(`${discordApiBase}/oauth2/token`, {
-    method: "POST",
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded",
-    },
-    body: params,
-  });
-  return response.json();
+  try {
+    let response = await fetch(`${discordApiBase}/oauth2/token`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+      body: params,
+    });
+    if (!response.ok) return undefined;
+    else return response.json();
+  } catch (err) {
+    return undefined;
+  }
 };
 
 const discordApiCall = async (resource, discordAuthToken) => {
-  let response = await fetch(`${discordApiBase}/${resource}`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${discordAuthToken}`,
-    },
-  });
-  return response.json();
-};
-
-const discordCodeRefresh = async (refreshToken) => {
-  let response = await fetch(`${discordApiBase}/oauth2/token`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: JSON.stringify({
-      client_id: clientId,
-      client_secret: clientSecret,
-      grant_type: "refresh_token",
-      refresh_token: refreshToken,
-    }),
-  });
-  return response.json();
+  try {
+    let response = await fetch(`${discordApiBase}/${resource}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${discordAuthToken}`,
+      },
+    });
+    if (!response.ok) return undefined;
+    else return response.json();
+  } catch (err) {
+    return undefined;
+  }
 };
 
 export const discordAuth = functions.https.onCall(async (data, context) => {
+  // parameter validation
+  if (!data.authCode)
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "authCode not provided"
+    );
+  if (!data.redirectUri)
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "redirectUri not provided"
+    );
+
   // discord access token exchange
   const discordToken = await discordCodeExchange(
     data.authCode,
-    "http://localhost:5173"
-  ); // TODO redirect uri
-  console.log("discordToken:", discordToken);
+    data.redirectUri
+  );
+  if (!discordToken)
+    throw new functions.https.HttpsError(
+      "permission-denied",
+      "could not sign in to discord with the auth token"
+    );
 
   // gather relevant userinfo
   const userinfo = await discordApiCall(
     "/users/@me",
     discordToken["access_token"]
   );
+  if (!userinfo)
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "could not request userinfo from the discord api"
+    );
+
+  // gather guilds a user is member of
   const guilds = await discordApiCall(
     "users/@me/guilds",
     discordToken["access_token"]
   );
-
-  console.log("userinfo:", userinfo);
-  console.log("guilds:", guilds);
+  if (!guilds)
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "could not request guilds from the discord api"
+    );
 
   const userId = userinfo["id"];
   const additionalClaims = {
     guilds: guilds.map((x) => x["id"]),
   };
-  // TODO can i use this token to directly authenticate the caller?
   const jwtToken = await createJwtToken(userId, additionalClaims);
   if (jwtToken) {
     return jwtToken;
@@ -97,24 +114,7 @@ const createJwtToken = async (userId, additionalClaims) => {
   try {
     return auth.createCustomToken(userId, additionalClaims);
   } catch (error) {
-    console.log("Error creating custom token:", error);
+    console.error("Error creating custom token:", error);
     return undefined;
   }
 };
-
-// not needed if i put the allowed servers in the additional claims of the createJwtToken
-// const createFirebaseUser = () => {
-//   auth
-//     .createUser({
-//       uid: "some-uid",
-//       email: "user@example.com",
-//       phoneNumber: "+11234567890",
-//       providerToLink,
-//     })
-//     .then((userRecord) => {
-//       console.log("Successfully created new user:", userRecord.uid);
-//     })
-//     .catch((error) => {
-//       console.log("Error creating new user:", error);
-//     });
-// };
