0568608e61cabb5885171fb5c20d48dbae2fb287
Lukas <boehm-lukas@outlook.de>
2022-09-20 14:41:12 +0200

    :bug: fix certificate login issue in bot'

--- bot/src/firebase.js
+++ bot/src/firebase.js
@@ -1,8 +1,10 @@
-import { initializeApp } from "firebase-admin/app";
+import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import serviceAccount from "../serviceAccount.json" assert { type: "json" };
 
-const app = initializeApp(serviceAccount);
+const app = initializeApp({
+  credential: cert(serviceAccount),
+});
 const firestore = getFirestore(app);
 
 export { firestore };
